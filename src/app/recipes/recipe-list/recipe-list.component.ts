b657ded47435eb4fc698f270ec9d8d3ee16cd391
Trinh Tran <kieutrinh.ibc@gmail.com>
2021-08-04 01:56:38 +0300

    router is added to recipe-edit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,8 +12,13 @@ import { Component, OnInit} from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[];
-  constructor(private RecipeService: RecipeService) { }
+  constructor(private RecipeService: RecipeService, 
+              private router: Router,
+              private route: ActivatedRoute) {}
   ngOnInit(){
     this.recipes = this.RecipeService.getRecipes();
+  }
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -48,6 +48,9 @@ export class RecipeService {
       getRecipes(){
           return this.recipes.slice();
       }
+      getRecipe(index:number){
+          return this.recipes[index];
+      }
 
       addIngredienttoShoppingList(ingredients:Ingredient[]){
           this.sLService.addIngredients(ingredients);
